e74110156f507da1a57447124f7ec529a4ac59a2
Kim Kunho <rlarmsrms93@naver.com>
2021-07-19 13:20:16 +0900

    temp fix

--- project/controllers/heradeeansuploader/ansuploader.py
+++ project/controllers/heradeeansuploader/ansuploader.py
@@ -1,8 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from project.modules.imagePillow import pilimgs3, pilimg
 
 bp = Blueprint('ansupload', __name__, url_prefix='/')
 
 
 @bp.route('/', methods=['GET', 'POST'])
 def upload():
-    return render_template('upload.html')
+    if request.method == 'GET':
+        return render_template('upload.html')
+
+    elif request.method == 'POST':
+        if request.form.get('action') == 'anssubmit':
+
+            # SLIDE
+            for i in range(1, 9):
+                if not request.files['slide' + str(i)]:
+                    pass
+                else:
+                    pilimgs3(request.files['slide' + str(i)],
+                             pilimg(request.files['slide' + str(i)]).size[0],
+                             pilimg(request.files['slide' + str(i)]).size[1],
+                             f'ans/{request.form.get("ans_account")}/slide/',
+                             f'{i}.jpg')
+
+            # PROFILE
+            if request.files['profile']:
+                pilimgs3(request.files['profile'],
+                         pilimg(request.files['profile']).size[0],
+                         pilimg(request.files['profile']).size[1],
+                         f'ans/{request.form.get("ans_account")}/profile/',
+                         '1.jpg')
+
+            if request.form.get('aboutme'):
+
+
+
+
+            return render_template('upload.html')
+
